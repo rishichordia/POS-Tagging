8dd1036a670211b90b0f8599a3d6b2bcd80650df
sunnysideup <kssuraaj28@gmail.com>
2020-11-03 22:55:14 +0400

    Add naive count of pairs TODO: Deal with cases with multiple tags
    Signed-off-by: sunnysideup <kssuraaj28@gmail.com>

---
+++ Pair.py
@@ -0,0 +1,24 @@
+from sys import argv
+import glob
+import pickle
+
+def main():
+	'''Usage: ./Pair.py'''
+	input_list=glob.glob("Train-corpus/*/*.txt")
+	pair_count_dict={}
+	for istr in input_list:
+		with open(istr) as f:
+			line=f.read()
+		words = line.split()
+		tags = [word.split('_')[1] for word in words]
+		for i in range(len(tags)-1): #TODO: Multiple tags?
+			pair = (tags[i],tags[i+1])
+			if pair not in pair_count_dict:
+				pair_count_dict[pair] = 0
+			pair_count_dict[pair] += 1
+
+	with open('res/pair.pkl', 'wb') as f:
+		pickle.dump(pair_count_dict, f, pickle.HIGHEST_PROTOCOL)
+
+if __name__ == '__main__':
+    main()
